0dfba1f24f004d607863fec76b768fdf3fdaaf35
Pozovi23 <gleb.zhigalov.2005@mail.ru>
2025-05-14 15:53:15 +0700

    readme edit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("best.pt")
-model.export(format="onnx", dynamic=False, simplify=True, opset=12, imgsz=(640, 640))
+model.export(format="onnx", dynamic=False, opset=12, imgsz=(640, 640))
